f49a8e21a54f27fcf1bb3fc7d5ec4eded7f81d9d
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-09-24 19:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+// Input array of band names
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd', 'Queen'];
 
+// Function to remove articles and return a modified string
+function removeArticles(name) {
+    return name.replace(/^(a|an|the) /i, '').trim();
+}
+
+// Remove articles and sort the band names
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Get the ul element with id "band"
+const bandList = document.getElementById('band');
+
+// Create list items and append them to the ul
+bandNames.forEach((bandName) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = bandName;
+    bandList.appendChild(listItem);
+});
